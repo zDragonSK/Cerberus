aead5a8b0574a01e10554256e36633049fea933d
zDragonSK <zdragonsk@pm.me>
2024-06-11 16:03:30 -0300

    Add swap point function

--- src/cerberus.c
+++ src/cerberus.c
@@ -11,9 +11,7 @@ int encript (const char *keyword, const char *keysimple){
   unsigned char *m_hash = MD5((const unsigned char *)keysimple, strlen(keysimple), NULL);
   if (!hash || !m_hash) return -1;
 
-  for (register int x=0; x<strlen(keysimple); x++)
-    if (keysimple[x]) hash[x] = m_hash[x];
-
+  swap(hash, m_hash, keysimple);
   printf("Key: %s\nTag: %s\nHash: ", keyword, keysimple);
   for (register int y=0; y<32; y++)
     printf("%02x", hash[y]);
@@ -30,7 +28,20 @@ int l_encript (const char *keyword) {
   printf("Key: %s\nHash: ", keyword);
   for (register int x=0; x<32; x++)
     printf("%02x", hash[x]);
-
   printf("\n");
+
   return 0;
+}
+
+/* Modify SHA256 hash using MD5 */
+void swap(unsigned char *sha256_hash, const unsigned char *md5_hash, const char *keysimple) {
+  size_t len = strlen(keysimple);
+  for (size_t i = 0; i < len; i++) {
+    size_t position = keysimple[i] % 32;
+    sha256_hash[position] = md5_hash[i % 16];
+  }
+  for (size_t i=0; i<len; i++){
+    size_t position = keysimple[i] % 21;
+    sha256_hash[position] = md5_hash[i % 16];
+  }
 }
--- src/lib/cerberus.h
+++ src/lib/cerberus.h
@@ -1,7 +1,8 @@
 /*
   Cerberus Header
   By: zDragonSK
-*/
+ */
+#include <stdlib.h>
 #ifndef _CERBERUS_H
 #define _CERBERUS_H
 
@@ -9,4 +10,6 @@
 int encript (const char *keyword, const char *keysimple);
 /* Encript a keyword without single-tag */
 int l_encript (const char *keyword);
+/* Swap a hash */
+void swap(unsigned char *sha256_hash, const unsigned char *md5_hash, const char *keysimple);
 #endif
